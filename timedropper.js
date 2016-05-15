e123c7bb9f28f42df270b3bce456e78f69a05591
janisdd <janisdd@web.de>
2016-05-15 16:57:15 +0200

    added umd support (require('timeDropper') should work)

--- timedropper.js
+++ timedropper.js
@@ -1,4 +1,33 @@
-(function($) {
+//https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module. 
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS 
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals 
+        factory(window.jQuery); // || window.Zepto ??
+    }
+}(function($) {
     $.fn.timeDropper = function(options, callbackFnk) {
         return $(this).each(function() {
 
@@ -416,4 +445,4 @@
 
         });
     };
-}(jQuery));
+}))
