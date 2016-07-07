acf4bfbb7a98b98aa5fbd84c16f724e6b5bd8ac6
H3xx3n <H3xx3n>
2016-07-07 15:28:48 +0200

    Added minutes interval, change event, hidden lancets option, fixed alert using mousewheel, fixed setCurrentTime

--- timedropper.js
+++ timedropper.js
@@ -23,7 +23,9 @@
                     primaryColor: "#1977CC",
                     borderColor: "#1977CC",
                     backgroundColor: "#FFF",
-                    textColor: '#555'
+                    textColor: '#555',
+					minutesInterval: 1,
+					showLancets: true
 
                 }, options);
 
@@ -63,7 +65,7 @@
 
             $('body').append('<div class="td-wrap td-n2" id="td-clock-' + _td_id + '"><div class="td-overlay"></div><div class="td-clock td-init"><div class="td-deg td-n"><div class="td-select"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 35.4" enable-background="new 0 0 100 35.4" xml:space="preserve"><g><path fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M98.1,33C85.4,21.5,68.5,14.5,50,14.5S14.6,21.5,1.9,33"/><line fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="1.9" y1="33" x2="1.9" y2="28.6"/><line fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="1.9" y1="33" x2="6.3" y2="33"/><line fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="98.1" y1="33" x2="93.7" y2="33"/><line fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="98.1" y1="33" x2="98.1" y2="28.6"/></g></svg></div></div><div class="td-medirian"><span class="td-icon-am td-n">AM</span><span class="td-icon-pm td-n">PM</span></div><div class="td-lancette"><div></div><div></div></div><div class="td-time"><span class="on"></span>:<span></span></div></div></div>');
 
-            $('head').append('<style>#td-clock-' + _td_id + ' .td-clock {color:' + _td_options.textColor + ';background: ' + _td_options.backgroundColor + '; box-shadow: 0 0 0 1px ' + _td_options.borderColor + ',0 0 0 8px rgba(0, 0, 0, 0.05); } #td-clock-' + _td_id + ' .td-clock .td-time span.on { color:' + _td_options.primaryColor + '} #td-clock-' + _td_id + ' .td-clock:before { border-color: ' + _td_options.borderColor + '} #td-clock-' + _td_id + ' .td-select:after { box-shadow: 0 0 0 1px ' + _td_options.borderColor + ' } #td-clock-' + _td_id + ' .td-clock:before,#td-clock-' + _td_id + ' .td-select:after {background: ' + _td_options.backgroundColor + ';} #td-clock-' + _td_id + ' .td-lancette {border: 2px solid ' + _td_options.primaryColor + '; opacity:0.1}#td-clock-' + _td_id + ' .td-lancette div:after { background: ' + _td_options.primaryColor + ';} #td-clock-' + _td_id + ' .td-bulletpoint div:after { background:' + _td_options.primaryColor + '; opacity:0.1}</style>');
+            $('head').append('<style>#td-clock-' + _td_id + ' .td-clock {color:' + _td_options.textColor + ';background: ' + _td_options.backgroundColor + '; box-shadow: 0 0 0 1px ' + _td_options.borderColor + ',0 0 0 8px rgba(0, 0, 0, 0.05); } #td-clock-' + _td_id + ' .td-clock .td-time span.on { color:' + _td_options.primaryColor + '} #td-clock-' + _td_id + ' .td-clock:before { border-color: ' + _td_options.borderColor + '} #td-clock-' + _td_id + ' .td-select:after { box-shadow: 0 0 0 1px ' + _td_options.borderColor + ' } #td-clock-' + _td_id + ' .td-clock:before,#td-clock-' + _td_id + ' .td-select:after {background: ' + _td_options.backgroundColor + ';} ' + ( _td_options.showLancets ? (' #td-clock-' + _td_id + ' .td-lancette {border: 2px solid ' + _td_options.primaryColor + '; opacity:0.1}#td-clock-' + _td_id + ' .td-lancette div:after { background: ' + _td_options.primaryColor + ';}') : ('#td-clock-' + _td_id + ' .td-lancette {display:none;}' )) + ' #td-clock-' + _td_id + ' .td-bulletpoint div:after { background:' + _td_options.primaryColor + '; opacity:0.1}</style>');
 
 
 
@@ -103,9 +105,11 @@
                     var value = t.attr('data-id');
 
                     if (!value) value = 0;
-
+					
+					var degMinutes = Math.floor(deg/(360/60*_td_options.minutesInterval))*360/60*_td_options.minutesInterval;
+					
                     var h = Math.floor((deg * 24 / 360));
-                    var m = Math.floor((deg * 60 / 360));
+                    var m = Math.floor((degMinutes * 60 / 360));
 
                     if (t.index() == 0) {
 
@@ -136,8 +140,14 @@
                         t.attr('data-id', _td_num(m)).text(_td_num(m));
                     }
 
-                    _td_wheel_deg = deg;
-                    _td_c.find('.td-deg').css('transform', 'rotate(' + (deg) + 'deg)');
+                    _td_wheel_deg = deg;	
+
+					if(t.index() == 0){
+						_td_c.find('.td-deg').css('transform', 'rotate(' + (deg) + 'deg)');
+					}else{
+						_td_c.find('.td-deg').css('transform', 'rotate(' + (degMinutes) + 'deg)');
+					}
+                    
 
                     if (t.index() == 0) {
                         var c = (h * 360 / 12);
@@ -177,8 +187,7 @@
                         .replace(/\b(A)\b/g, A);
 
                     _td_input.val(str);
-
-
+					_td_input.change();
                 };
 
 
@@ -261,8 +270,8 @@
             if (_td_options.mousewheel) {
 
                 _td_c.on('mousewheel', function(e) {
-
                     e.preventDefault();
+					 clearInterval(_td_alert);
 
                     _td_c.find('.td-deg').removeClass('td-n');
 
@@ -315,11 +324,11 @@
                     h,
                     m;
 
-                if (_td_input.val().length && !_td_options.setCurrentTime) {
+                if (_td_options.setCurrentTime) {
 
                     var reg = /\d+/g,
                         am;
-                    var st = _td_input.val().split(':');
+                    var st = _td_input.val().indexOf(':')>-1?_td_input.val().split(':'):null;
 
                     if (st) {
 
@@ -347,9 +356,9 @@
 
                 } else {
 
-                    if (!parseInt(_td_span_h.text())) h = _td_num(d.getHours());
+                    if (!parseInt(_td_span_h.text())) h = '00';
                     else h = _td_num(_td_span_h.text());
-                    if (!parseInt(_td_span_m.text())) m = _td_num(d.getMinutes());
+                    if (!parseInt(_td_span_m.text())) m = '00';
                     else m = _td_num(_td_span_m.text());
 
                 }
